f689c58adf93076177e0e3a0a036ad0aa4f13153
matthias <matthias@192.168.0.211>
2013-06-07 17:19:46 +0200

    task editing improved

--- public/js/views/task.js
+++ public/js/views/task.js
@@ -31,39 +31,75 @@ define(['mt.backbone.sio', 'mt.templates', 'models/task'], function(Backbone, te
             }
         },
         events: {
-            "click div": "click",
+            "click": "click",
+            "keydown": "keyShotcuts",
+            "keypress": "keyShotcuts",
             "keyup": "onChange"
         },
         click: function(event) {
             if (!this.editMode) {
                 this.eventBus.trigger("blur", this);
                 this.switchEditable();
-                return false;
+            }
+            this.setFocusOnEditableDiv(event.target);
+        },
+        setFocusOnEditableDiv: function(element) {
+            if (element !== document) {
+                if (element.getAttribute('contenteditable') === 'true') {
+                    element.focus();
+                } else {
+                    this.setFocusOnEditableDiv(element.parentNode);
+                }
+            }
+        },
+        keyShotcuts: function(event) {
+            // title specific events
+            if ((event.target.className.indexOf('mt-task-title') > -1)
+                    || ($(event.target).parents('.mt-task-title').length !== 0)) {
+                // return, tab -> switch to text field
+                if (event.keyCode === 13 || event.keyCode === 9) {
+                    if ($(this.el).find(this.text).attr('contenteditable') !== 'true') {
+                        $(this.el).find(this.text).attr('contenteditable', 'true');
+                    }
+                    $(this.el).find(this.text).focus();
+                    return false;
+                }
+            }
+            // text specific events
+            if ((event.target.className.indexOf('mt-task-text') > -1)
+                    || ($(event.target).parents('.mt-task-text').length !== 0)) {
+                // tab -> switch to title field
+                if (event.keyCode === 9) {
+                    $(this.el).find(this.title).focus();
+                    return false;
+                }
             }
         },
         onChange: function(event) {
+
+            // save changed content
+            var self = this;
             this.model.set('title', $(this.el).find(this.title).html());
             this.model.set('text', $(this.el).find(this.text).html());
             if (this.model.get('id') === 'new' && !this.hasChanged) {
                 if (this.model.isValid()) {
-                    this.model.save({}, {
+                    this.model.save(null, {
                         success: function(model, response, options) {
-                            this.model.set('id', '' + response);
-                            this.$el.attr('id', 'task-' + this.model.get('id'));
+                            self.model.set('id', '' + response);
+                            self.$el.attr('id', 'task-' + self.model.get('id'));
                         }, error: function(response) {
-                      console.log(response);
-                          alert("Task save failed!");
+                            console.log(response);
+                            alert("Task save failed!\ncheck console for details.");
                         }
                     });
-
                     return;
-                    // todo
                 }
             }
             this.hasChanged = true;
             this.model.save(null, {
                 success: function(model, response, options) {
-                }, error: function(model, response, options) {
+                },
+                error: function(model, response, options) {
                     console.log(response);
                 }
             });
@@ -82,15 +118,15 @@ define(['mt.backbone.sio', 'mt.templates', 'models/task'], function(Backbone, te
             if (on) {
                 this.editMode = true;
                 $(this.el).find(this.title).attr('contenteditable', 'true');
-                $(this.el).find(this.text).attr('contenteditable', 'true');
-                $(this.el).find(this.title).css('backgroundColor', '#EAFFEA');
-                $(this.el).find(this.text).css('backgroundColor', '#EAFFEA');
+                if (this.model.get("text")) {
+                    $(this.el).find(this.text).attr('contenteditable', 'true');
+                }
+                $(this.el).find('.mt-task-edit-mode').fadeIn(200);
             } else {
                 this.editMode = false;
                 $(this.el).find(this.title).attr('contenteditable', 'false');
                 $(this.el).find(this.text).attr('contenteditable', 'false');
-                $(this.el).find(this.title).animate({'backgroundColor': 'white'});
-                $(this.el).find(this.text).animate({'backgroundColor': 'white'});
+                $(this.el).find('.mt-task-edit-mode').fadeOut(400);
             }
         },
         blur: function(initiator) {
