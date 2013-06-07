ba55fef3d47bb47c07445454223c83d129618064
matthias <matthias@192.168.0.211>
2013-06-07 14:13:58 +0200

    prevent multiple concurrent saves

--- public/js/mt.backbone.sio.js
+++ public/js/mt.backbone.sio.js
@@ -13,30 +13,36 @@ define(['backbone', 'mt.socket'], function(Backbone, socket) {
 
     Backbone.sync = function(method, model, options) {
         var error, socket, success, _ref;
-        socket = model.socket || ((_ref = model.collection) !== null ? _ref.socket : void 0);
-        if (!socket) {
-            return originalSync.apply(this, arguments);
-        }
-        success = options.success;
-        delete options.success;
-        error = options.error;
-        delete options.error;
-        var modelUrl = (typeof model.url === 'function') ? model.url() : model.url;
-        socket.emit("backbone.sync", {
-            url: modelUrl,
-            method: method,
-            model: model,
-            options: options
-        }, function(err, response) {
-            if (err) {
-                error.call(this, err);
-            } else {
-                success.call(this, response);
+
+        if (!model._saving) {
+            model._saving = true;
+            socket = model.socket || ((_ref = model.collection) !== null ? _ref.socket : void 0);
+            if (!socket) {
+                return originalSync.apply(this, arguments);
             }
-        });
+            success = options.success;
+            delete options.success;
+            error = options.error;
+            delete options.error;
+            var modelUrl = (typeof model.url === 'function') ? model.url() : model.url;
+            socket.emit("backbone.sync", {
+                url: modelUrl,
+                method: method,
+                model: model,
+                options: options
+            }, function(err, response) {
+                if (err) {
+                    error.call(this, err);
+                } else {
+                    success.call(this, response);
+                }
+                model._saving = false;
+            });
+        }
     };
 
     Backbone.Model = Backbone.Model.extend({
+        _saving: false,
         constructor: function(attributes, options) {
             this.socket = socket;
             originalModel.apply(this, arguments);
