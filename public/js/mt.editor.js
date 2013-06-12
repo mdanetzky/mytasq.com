11ccf8ab09df451d7b3e78c550621d3784aa4833
matthias <matthias@192.168.0.211>
2013-06-12 18:04:33 +0200

    created ckeditor toolbar container

--- public/js/mt.editor.js
+++ public/js/mt.editor.js
@@ -9,30 +9,77 @@ define(['mt.backbone.sio', 'jquery', 'ckeditor'], function(Backbone, $) {
     var editors = {};
 
     return {
-        show: function(id, element, $toolbarTarget) {
+        show: function(id, element, $toolbarTarget, callback) {
+            var self = this;
             var editor = editors[id];
             if (!editor) {
                 var options = {};
-                editor = CKEDITOR.inline(element, options);
-
-
-                editor.on('focus', function(event)
+                editor = {};
+                editor.ck = CKEDITOR.inline(element, options);
+                editor.$toolbarTarget = $toolbarTarget;
+                editor.ck.on('focus', function(event)
                 {
-                    // Cut out the CK toolbar and move it to task
+                    // Cut out the CK toolbar and move it to task.
                     var $toolbar = $('#cke_' + event.editor.name + ' .cke_inner');
-                    $toolbarTarget.append($toolbar);
-                    
+                    editor.$toolbarTarget.append($toolbar);
+                    self.swingIn(editor);
+                    if (callback) {
+                        callback();
+                    }
                 });
                 editors[id] = editor;
+            } else {
+                clearTimeout(editor.destroyTimeout);
+                if (!editor.isVisible) {
+                    editor.$toolbarTarget.parent().stop(true, true);
+                    self.swingIn(editor, callback);
+                    if (callback) {
+                        callback();
+                    }
+                }
+            }
+            editor.isVisible = true;
+        },
+        swingIn: function(editor) {
+            // Toolbar: Slide in from top.
+            var toolbarHeight = editor.$toolbarTarget.outerHeight(true);
+            editor.$toolbarTarget.parent().animate({
+                height: toolbarHeight,
+                "margin-bottom": 10
+            }, 'fast', 'swing', function() {
+                editor.$toolbarTarget.parent().css('height', '');
+            });
+        },
+        hide: function(id) {
+            var editor = editors[id];
+            if (editor && editor.isVisible) {
+                editor.$toolbarTarget.parent().stop(true, true);
+                editor.$toolbarTarget.parent().animate({
+                    height: 0,
+                    "margin-bottom": 0
+                }, 'fast');
+                editor.isVisible = false;
+
+                // Destroy unused editor instances 30 seconds after hide
+                var self = this;
+                editor.destroyTimeout = setTimeout(function() {
+                    self.destroy(id);
+                }, 30000);
             }
         },
-        hide: function(element) {
-
-        },
-        getData: function(element) {
-            var editor = editors.get(element);
+        getData: function(id) {
+            var editor = editors[id];
             if (editor) {
-                return editor.getData();
+                return editor.ck.getData();
+            }
+        },
+        destroy: function(id) {
+            var editor = editors[id];
+            if (editor) {
+                clearTimeout(editor.destroyTimeout);
+                editor.ck.destroy();
+                editor.$toolbarTarget.empty();
+                delete editors[id];
             }
         }
     };
