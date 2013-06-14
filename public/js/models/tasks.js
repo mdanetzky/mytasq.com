86d9dff1f2cd59ebfbfb985b6840971adff5f433
matthias <matthias@192.168.0.211>
2013-06-14 09:50:01 +0200

    Cleaned comment

--- public/js/models/tasks.js
+++ public/js/models/tasks.js
@@ -1,6 +1,8 @@
 /* 
  * Copyright 2013 MyTasq.com
  * Author: Matthias Danetzky
+ * 
+ * Backbone tasks collection
  */
 
 define(['mt.backbone.sio', 'jquery', 'models/task' ], function(Backbone, $, Task){
