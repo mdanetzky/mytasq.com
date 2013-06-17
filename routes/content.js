1b6ca0494b57aa1645a7693d50d0729cb1b8598d
matthias <matthias@192.168.0.211>
2013-06-17 19:50:08 +0200

    Code and comments cleanup.

--- routes/content.js
+++ routes/content.js
@@ -2,12 +2,11 @@
  * Copyright 2013 MyTasq.com
  * Author: Matthias Danetzky
  * 
- * main content route
+ * Main content route.
  */
 
 var contentController = require('../controllers/content')
         ;
-
 exports = module.exports = function(req, res) {
     contentController.mainContent(req, function(err, renderingContext) { 
         res.render('content.html', renderingContext);
