3e0a2959fdf90e480ae36a3619ba9ff7b51647cd
matthias <matthias@192.168.0.211>
2013-06-17 19:43:07 +0200

    Top navigation panel handler added.

---
+++ public/js/views/nav.top.js
@@ -0,0 +1,30 @@
+/* 
+ * Copyright 2013 MyTasq.com
+ * Author: Matthias Danetzky
+ * 
+ * Bootstrap view.
+ * Top navigation panel.
+ */
+
+define(['mt.backbone.sio', 'jquery'], function(Backbone, $) {
+    var NavTopView = Backbone.View.extend({
+        el: '#mt-navbar',
+        initialize: function() {
+
+        },
+        events: {
+            "click #mt-nav-done-by-me": "tasksDoneByMe",
+            "click #new_task": "createTask"
+        },
+        tasksDoneByMe: function() {
+            this.options.app.trigger('showDoneByMe');
+            $('.dropdown.open .dropdown-toggle').dropdown('toggle');
+            return false;
+        },
+        createTask: function() {
+            this.options.app.trigger('createTask');
+            return false;
+        }
+    });
+    return NavTopView;
+});
