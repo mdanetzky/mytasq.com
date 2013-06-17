21664cc8d8f128ed9b25a5e0dba7fb4a5fb49197
matthias <matthias@192.168.0.211>
2013-06-17 19:48:53 +0200

    Code and comments cleanup.

--- public/js/views/nav.left.js
+++ public/js/views/nav.left.js
@@ -2,12 +2,11 @@
  * Copyright 2013 MyTasq.com
  * Author: Matthias Danetzky
  * 
- * Backbone view
- * Left navigation panel
+ * Backbone view.
+ * Left navigation panel.
  */
 
 define(['jquery', 'views/scrollable'], function($, ScrollableView) {
-
     var NavLeftView = ScrollableView.extend({
         el: '#mt-left-nav-container',
         viewport: '#mt-left-nav-viewport',
@@ -15,6 +14,5 @@ define(['jquery', 'views/scrollable'], function($, ScrollableView) {
             railoffset: {left: 12}
         }
     });
-
     return NavLeftView;
 });
--- public/js/views/nav.right.js
+++ public/js/views/nav.right.js
@@ -2,16 +2,14 @@
  * Copyright 2013 MyTasq.com
  * Author: Matthias Danetzky
  * 
- * Backbone view
- * Right navigation panel
+ * Backbone view.
+ * Right navigation panel.
  */
 
 define(['jquery', 'views/scrollable'], function($, ScrollableView) {
-    
     var NavRightView = ScrollableView.extend({
         el: '#mt-right-nav-container',
         viewport: '#mt-right-nav-container'
     });
-
     return NavRightView;
 });
