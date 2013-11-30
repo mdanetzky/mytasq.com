e5591c5b98c102d8f61471d181cf4d010b988fc7
matthias <matthias@192.168.0.211>
2013-11-30 19:00:42 +0100

    jsbeautified

--- app.js
+++ app.js
@@ -5,17 +5,16 @@
  * Author: Matthias Danetzky
  */
 
-var express = require('express')
-        , cluster = require('cluster')
-        , routes = require('./routes')
-        , http = require('http')
-        , path = require('path')
-        , mongoose = require('mongoose')
-        , engines = require('consolidate')
-        , util = require('util')
-        , conf = require('./lib/mt.conf')
-        , log = require('./lib/mt.logger')(module)
-        ;
+var express = require('express'),
+    cluster = require('cluster'),
+    routes = require('./routes'),
+    http = require('http'),
+    path = require('path'),
+    mongoose = require('mongoose'),
+    engines = require('consolidate'),
+    util = require('util'),
+    conf = require('./lib/mt.conf'),
+    log = require('./lib/mt.logger')(module);
 log.info('#################################');
 log.info('START: mytasq.com server instance');
 // precompile templates at start.
@@ -26,18 +25,18 @@ if (conf.err) {
     process.exit(1);
 }
 if (conf.development) {
-    mongoose.set('debug', function(collectionName, method, query, doc) {
+    mongoose.set('debug', function (collectionName, method, query, doc) {
         log.log('debug', 'Mongoose collection: "' + collectionName + '" method: "' + method + '" query: ', query);
     });
 }
 // Prepare logStream for express.
 var logStream = {
-    write: function(message){
+    write: function (message) {
         log.info(message);
     }
 };
 // Connect to the database.
-mongoose.connect(conf.db.uri, conf.db.options, function(err) {
+mongoose.connect(conf.db.uri, conf.db.options, function (err) {
     if (err) {
         console.log(conf.err);
         process.exit(1);
@@ -58,43 +57,52 @@ if (cluster.isMaster && !conf.singleThread) {
 } else {
     // Start Express app in worker process.
     var app = express();
-    app.configure(function() {
+    app.configure(function () {
         app.engine('.jade', engines.jade);
         app.engine('.html', engines.underscore);
         app.set('port', process.env.PORT || 80);
         app.set('views', __dirname + '/views');
         app.use(express.static(conf.staticPathAbsolute));
-//      app.use(express.favicon(path.join(__dirname, 'public', 'favicon.ico'), {maxAge: 1}));
+        //      app.use(express.favicon(path.join(__dirname, 'public', 'favicon.ico'), {maxAge: 1}));
         app.use(express.bodyParser());
         app.use(express.methodOverride());
         app.use(express.cookieParser());
         app.use(express.session({
             secret: conf.session.secret,
             key: conf.session.key,
-            'cookie': {expires: false, maxAge: 365 * 24 * 60 * 60 * 1000}, // one year
+            'cookie': {
+                expires: false,
+                maxAge: 365 * 24 * 60 * 60 * 1000
+            },
+            // one year
             store: sessionStore
         }));
         app.use(app.router);
-        app.use(express.logger({stream: logStream}));
+        app.use(express.logger({
+            stream: logStream
+        }));
         require('./lib/mt.passport')(app);
     });
-    app.configure('development', function() {
-        app.use(express.errorHandler({dumpExceptions: true, showStack: true}));
+    app.configure('development', function () {
+        app.use(express.errorHandler({
+            dumpExceptions: true,
+            showStack: true
+        }));
     });
-    app.configure('production', function() {
+    app.configure('production', function () {
         app.use(express.errorHandler());
     });
     // Routes.
     app.get('/', routes.home);
     app.get('/testdata', routes.testdata);
     app.get('/content', routes.content);
-    app.post('/dummy', function(req, res) {
+    app.post('/dummy', function (req, res) {
         res.send('');
     });
-    app.get('/dummy', function(req, res) {
+    app.get('/dummy', function (req, res) {
         res.send('');
     });
-    var server = http.createServer(app).listen(app.get('port'), function() {
+    var server = http.createServer(app).listen(app.get('port'), function () {
         log.info("Express server listening on port " + app.get('port'));
     });
     // Socket event handlers.
