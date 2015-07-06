6258ba3629b5c991b0d618435bd707042e1da30f
matthias <matthias@192.168.0.10>
2015-07-06 13:15:40 +0200

    user tasks field added

---
+++ models/userTask.js
@@ -0,0 +1,25 @@
+/* 
+ * Copyright 2013 MyTasq.com
+ * Author: Matthias Danetzky
+ * 
+ * Mongoose Model.
+ * Task.
+ */
+
+var mongoose = require('mongoose')
+        , Schema = mongoose.Schema
+        , User = require('./user')
+        ;
+var TaskSchema = new Schema({
+    parentTask: {type: Schema.ObjectId, ref: 'Task', index: true}
+    , title: {type: String, default: '', index: true}
+    , text: {type: String, default: ''}
+    , author: {type: Schema.ObjectId, ref: 'User', index: true}
+    , assignee: {type: Schema.ObjectId, ref: 'User', sparse: true}
+    , cretionTime: {type: Date, default: Date.now}
+    , lastModifiedTime: {type: Date, default: Date.now, index: true}
+    , public: {type: Boolean, default: false, index: true}
+    , done: {type: Boolean, default: false, index: true}
+    , deleted: {type: Boolean, required: true, default: false, index: true}
+}, {collection: "mtTasks"});
+module.exports = mongoose.model('Task', TaskSchema);
